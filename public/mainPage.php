f2186d16a8bd766080d79505a939017984c9df88
caesarDapiton <dapitoncaesar26@gmail.com>
2024-11-01 13:58:31 +0800

    header and embedding the main report to the web

--- public/mainPage.php
+++ public/mainPage.php
@@ -9,7 +9,9 @@
     <title>Main Page</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="min-h-screen bg-slate-50 dark:bg-white dark:text-white">
-    <h1>This is the main page</h1>
+<body>
+    <div class="h-vh w-screen">
+        <iframe title="gisBalagtas01" class="w-full h-full" src="https://app.powerbi.com/reportEmbed?reportId=c80d7c2e-5e20-464b-a6a4-3af84f4bf561&autoAuth=true&ctid=636e71dd-925d-405a-a507-d51f91bd866c" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
 </body>
 </html>
---
+++ public/samplePage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<iframe title="gisBalagtas01" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=c80d7c2e-5e20-464b-a6a4-3af84f4bf561&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>
+</body>
+</html>
