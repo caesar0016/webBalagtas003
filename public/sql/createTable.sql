b693cf6fe35458c94b63c421b2c6c500aabe896a
caesar0016 <94184309+caesar0016@users.noreply.github.com>
2024-12-03 18:25:56 +0800

    Update createTable.sql

--- public/sql/createTable.sql
+++ public/sql/createTable.sql
@@ -10,10 +10,17 @@ create table tblAccount(
 );
 
 insert into tblAccount(name, username, password, accountType) 
-    values ('Admin', 'admin0016', '$2y$10$fxBvX4uAa28gwlRebvtOs.3yzU9FOQfxp4l4GYECI0SE1SR7nvafW');
+    values ('Admin', 'admin0016', '$2y$10$fxBvX4uAa28gwlRebvtOs.3yzU9FOQfxp4l4GYECI0SE1SR7nvafW', 'Admin');
 
 Select * from tblAccount
 
-update tblAccount
-set archiveFlag = 2
-where
+DROP TABLE "Data 1", "Data 2", "Data 3";  --drop table
+
+
+--this query shows all of the table inside the database
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+ORDER BY table_name;
+
+
